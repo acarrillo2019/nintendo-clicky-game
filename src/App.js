40f5c0cda3592b10593155c82f2de287e5e25a12
acarrillo2019 <alric21@aol.com>
2020-04-17 05:48:18 -0700

    Added component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import images from "./images.json";
+import ImageCard from "./components/ImageCard";
+import Wrapper from "./components/Wrapper";
+shuffle(images);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
 }
 
 export default App;
