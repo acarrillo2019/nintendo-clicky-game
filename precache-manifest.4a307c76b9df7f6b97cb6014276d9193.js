f15bfa5a3a68614d98839bdd2e5716d2f327845d
acarrillo2019 <alric21@aol.com>
2020-04-17 22:36:47 -0700

    Updates

---
+++ precache-manifest.4a307c76b9df7f6b97cb6014276d9193.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d673888ce1b0028df09c929ed3df47b3",
+    "url": "/nintendo-clicky-game/index.html"
+  },
+  {
+    "revision": "249d9144404c1bd0bb22",
+    "url": "/nintendo-clicky-game/static/css/main.d8ab594d.chunk.css"
+  },
+  {
+    "revision": "87beb58a5bd21857dbf8",
+    "url": "/nintendo-clicky-game/static/js/2.7b786aab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nintendo-clicky-game/static/js/2.7b786aab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "249d9144404c1bd0bb22",
+    "url": "/nintendo-clicky-game/static/js/main.c2c1c341.chunk.js"
+  },
+  {
+    "revision": "abca3d959ac609ad5eb2",
+    "url": "/nintendo-clicky-game/static/js/runtime-main.13e82dd4.js"
+  },
+  {
+    "revision": "0603a1ff64e814f5ce524392ef371572",
+    "url": "/nintendo-clicky-game/static/media/smb_coin.0603a1ff.mp3"
+  },
+  {
+    "revision": "6fd4222800a8259e741dc228ad7c1814",
+    "url": "/nintendo-clicky-game/static/media/smb_mariodie.6fd42228.mp3"
+  }
+]);
