51be88289f6da5314c1cc29c43f87e013fc04717
acarrillo2019 <alric21@aol.com>
2020-04-17 22:29:09 -0700

    reset

---
+++ src/components/Marker/index.js
@@ -0,0 +1,36 @@
+import React from "./node_modules/react";
+import "./style.css";
+const Logo = require('./nintendo-logo.png');
+
+function Marker(props) {
+  return (
+    <>
+      <div className="container-fluid game">
+        <div className="navbar fixed-top gamenav">
+          <div className={props.class} onAnimationEnd={props.anim}>
+            {props.text}
+          </div>
+          
+          <a className="title" href="/">
+            <img className="logo" src={Logo}/>
+          </a>
+          
+          <div>
+            <span>Score: {props.score}</span> |
+            <span> Top score: {props.topScore}</span>
+          </div>
+        </div>
+        {/* <div className="bg">
+          <h2>Clicky Game!</h2> Click on an image to earn points, but don't
+          click on an image twice!
+        </div> */}
+        <div className={`Marker ${props.class}`}>{props.children}</div>
+      </div>
+      <div className="footer">
+      <img className="logo" src={Logo}/>
+      </div>
+    </>
+  );
+}
+
+export default Marker;
